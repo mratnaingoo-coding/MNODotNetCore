f06176bae23c8029c29ff583b43e6e221c498d6c
MratRikzil <142309328+MratRikzil@users.noreply.github.com>
2024-04-21 21:53:10 +0630

    Complete testing.

--- MNODotNetCore.ConsoleApp/HelloWorld.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MNODotNetCore.ConsoleApp;
-
-public class HelloWorld
-{
-    public void Equation(int x, int y){
-            int z = (x*y) + y;
-            System.Console.WriteLine("The result: "+z);
-    }
-}
