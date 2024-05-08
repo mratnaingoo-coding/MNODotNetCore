e941c8776ccdbccc2a1eee93e26503ca568f95bf
MratRikzil <142309328+MratRikzil@users.noreply.github.com>
2024-05-08 20:55:08 +0630

    BirdControllerApi Complete.

---
+++ MNODotNetCore.ConsoleAppHttpClient/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
+
+Console.WriteLine("Hello, World!");
+
+string jsonTest = await File.ReadAllTextAsync("Birds.json");
+var model = JsonConvert.DeserializeObject<Birds>(jsonTest);
+/*
+Console.WriteLine(jsonTest);*/
+
+foreach (var item in model.Tbl_Bird)
+{
+    Console.WriteLine(item.BirdEnglishName);
+}
+
+static string ToNum(string num)
+{
+    num = num.Replace("1","၁")း;
+
+    return num;
+
+}
+
+
+Console.ReadLine();
+
+public class Birds
+{
+    public Tbl_Bird[] Tbl_Bird { get; set; }
+}
+
+public class Tbl_Bird
+{
+    public int Id { get; set; }
+    public string BirdMyanmarName { get; set; }
+    public string BirdEnglishName { get; set; }
+    public string Description { get; set; }
+    public string ImagePath { get; set; }
+}
+
+
+
+
+// json to C# -> install newtonsoft package 
---
+++ MNODotNetCore.RestAPIwithNLayer/Features/Birds/BirdsController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace MNODotNetCore.RestAPIwithNLayer.Features.Birds
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BirdsController : ControllerBase
+    {
+
+        private async Task<Birds> GetDataAsync()
+        {
+
+            string jsonTest = await System.IO.File.ReadAllTextAsync("Birds.json");
+            var model = JsonConvert.DeserializeObject<Birds>(jsonTest);
+            return model;
+        }
+        // api/Birds/types
+        [HttpGet("tbl_bird")]
+        public async Task<IActionResult> GetBirds()
+        {
+            var model = await GetDataAsync();
+            return Ok(model.Tbl_Bird);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> TypeOfBirds(int id)
+        {
+            var model = await GetDataAsync();
+            return Ok(model.Tbl_Bird.FirstOrDefault(x => x.Id == id));
+        }
+
+    }
+
+
+    public class Birds
+    {
+        public Tbl_Bird[] Tbl_Bird { get; set; }
+    }
+
+    public class Tbl_Bird
+    {
+        public int Id { get; set; }
+        public string BirdMyanmarName { get; set; }
+        public string BirdEnglishName { get; set; }
+        public string Description { get; set; }
+        public string ImagePath { get; set; }
+    }
+
+}
