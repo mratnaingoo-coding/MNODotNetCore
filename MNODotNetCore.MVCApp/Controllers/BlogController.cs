b310e0bee206fc34eade21693ca7ab2cf44b0f62
MratRikzil <142309328+MratRikzil@users.noreply.github.com>
2024-06-20 23:20:45 +0630

    ASP.Net Core Web MVC Blog Edit, Update & Delete[Complete]

--- MNODotNetCore.MVCApp/Controllers/BlogController.cs
+++ MNODotNetCore.MVCApp/Controllers/BlogController.cs
@@ -16,7 +16,9 @@ namespace MNODotNetCore.MVCApp.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var lst = await _db.Blog.ToListAsync();
+            var lst = await _db.Blog
+                .OrderByDescending(x => x.BlogID)
+                .ToListAsync();
             return View(lst);
         }
         [ActionName("Create")]
@@ -31,6 +33,52 @@ namespace MNODotNetCore.MVCApp.Controllers
             await _db.Blog.AddAsync(blog);
             var result = await _db.SaveChangesAsync();
             /* return View("BlogCreate");*/
+            return Redirect("/Blog");
+        }
+
+        [HttpGet]
+        [ActionName("Edit")]
+        public async Task<IActionResult> BlogEdit(int id)
+        {
+             var item = await _db.Blog.FirstOrDefaultAsync(x => x.BlogID == id);
+             if (item is null)
+            {
+                return Redirect("/Blog");
+            }
+             return View("BlogEdit",item);
+        }
+
+        [HttpPost]
+        [ActionName("Update")]
+        public async Task<IActionResult> BlogUpdate(int id, BlogModel blog)
+        {
+            var item = await _db.Blog.FirstOrDefaultAsync(x => x.BlogID == id);
+            if (item is null)
+            {
+                return Redirect("/Blog");
+            }
+
+            item.BlogTitle    = blog.BlogTitle;
+            item.BlogAuthor   = blog.BlogAuthor;
+            item.BlogContent  = blog.BlogContent;
+
+            await _db.SaveChangesAsync();
+            return Redirect("/Blog");
+        }
+
+        [HttpGet]
+        [ActionName("Delete")]
+        public async Task<IActionResult> BlogDelete(int id)
+        {
+            var item = await _db.Blog.FirstOrDefaultAsync(x => x.BlogID == id);
+            if (item is null)
+            {
+                return Redirect("/Blog");
+            }
+
+            _db.Blog.Remove(item);
+
+            await _db.SaveChangesAsync();
             return Redirect("/Blog");
         }
     }
